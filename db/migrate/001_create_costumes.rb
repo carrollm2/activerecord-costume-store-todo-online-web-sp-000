04a05341743b6b9f5bd051af52f41840ce955466
Michael Carroll <carrollm2@gmail.com>
2020-01-18 01:51:37 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -14,6 +14,8 @@ class CreateCostumes < ActiveRecord::Migration[5.2]
       t.float :price
       t.string :image_url
       t.string :size
+      t.datetime :created_at
+      t.datetime :updated_at
     end
   end
 end
